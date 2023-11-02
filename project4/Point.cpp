008d0533f3224a27a5c6a07cf16edfb081821111
hayasin <hayasin@umich.edu>
2023-11-01 21:32:19 -0400

    Made Point.CPP

--- project4/Point.cpp
+++ project4/Point.cpp
@@ -1,14 +1,14 @@
 /**
  * Point.cpp
- * Project UID 2e6ea4e086ea6a06753e819c30923369
  *
  * EECS 183
- * Project 4: CoolPics
+ * Lab 7
+ * Taken from Project 4: CoolPics
  *
- * <#Name(s)#>
- * <#uniqname(s)#>
+ * <#Name#> Hasan, Michael
+ * <#uniqname#>hayasin, madancea
  *
- * <#Description#>
+ * <#Description#> Finds coordinate for different points, determines if range is papropriate
  */
 
 #include "Point.h"
@@ -18,6 +18,76 @@
 
 // TODO: implement two constructors, setX, getX, setY, getY, read, write, checkRange.
 
+Point::Point() {
+    x = 0;
+    y = 0;
+}
+
+Point::Point(int xVal, int yVal) {
+    x = xVal;
+    y = yVal;
+}
+
+void Point::setX(int xVal){
+    x = xVal;
+}
+void Point::setY(int yVal){
+    y = yVal;
+}
+
+int Point::getX(){
+    return x;
+}
+int Point::getY(){
+    return y;
+}
+
+void Point::write(ostream& outs) {
+    //Ensuring that point x or y is within proper range
+    x = checkRange(x);
+    y = checkRange(y);
+    
+    outs.clear();
+    outs << '(' << x << ',' << ' ' << y << ')';
+
+    return;
+}
+
+void Point::read(istream& ins) {
+    ins.clear();
+    char a;
+    ins >> a >> x >> a >> y >> a;
+
+    return;
+}
+
+    // Check if the point is within a given range
+//int Point::checkRange(int val){
+//    if(val <= DIMENSION && val >= 0){
+//        return val;
+//    }
+//    else if (val < 0){
+//        val = 0;
+//        return val;
+//    }
+//    else{
+//        val = DIMENSION - 1;
+//        return val;
+//    }
+//
+//}
+
+int Point::checkRange(int val){
+    if(val < 0){
+        return 0;
+    }
+    else if (val >= DIMENSION){
+        return DIMENSION - 1;
+    }
+    else {
+        return val;
+    }
+}
 
 
 // Your code goes above this line.
